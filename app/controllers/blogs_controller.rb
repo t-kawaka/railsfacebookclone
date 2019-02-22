235fe26372905b082e2810dc899432104491ce0d
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-02-22 16:03:53 +0000

    authenticate修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-   before_action :authenticate_user!
+   before_action :authenticate_user! , only: [:edit, :update, :destroy]
   def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'blogs/home'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "blogs#home"
+  resources :blogs
 end
