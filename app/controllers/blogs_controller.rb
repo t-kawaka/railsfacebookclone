1c0e25fdb12bb65770d9cee4c3a61228bf46f4e4
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-02-23 05:03:05 +0000

    blog_controller修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,21 @@
 class BlogsController < ApplicationController
    before_action :authenticate_user! , only: [:edit, :update, :destroy]
   def home
+    @blogs = Blog.all
+  end
+  
+  def new
+    @blog = Blog.new
+  end
+  
+  def create
+    Blog.create(blog_params)
+    redirect_to new_blog_path
+  end
+  
+  private
+  
+  def blog_params
+    params.require(:blog).permit(:content, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "blogs#home"
+  root to: "blogs#index"
   resources :blogs
 end
