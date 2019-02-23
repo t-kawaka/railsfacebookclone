adb56706fcc82491656b21378885955e37d2c059
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-02-23 06:48:34 +0000

    edit機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
-   before_action :authenticate_user! , only: [:edit, :update, :destroy]
+   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def home
     @blogs = Blog.all
   end
@@ -9,13 +10,42 @@ class BlogsController < ApplicationController
   end
   
   def create
+    @blog = Blog.new(params)
+    if @blog.save
     Blog.create(blog_params)
-    redirect_to new_blog_path
+      redirect_to new_blog_path
+    else
+      render 'new'
+    end
   end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました"
+  end
+  
   
   private
   
   def blog_params
     params.require(:blog).permit(:content, :image)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
