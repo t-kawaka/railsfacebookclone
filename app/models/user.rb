8e361f0527392902f3122ea0349c1dbbc24d2824
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-02-23 14:08:03 +0000

    新規登録画面修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :blogs
+  validates :name, presence: true
 end
