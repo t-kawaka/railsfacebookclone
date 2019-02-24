25831ea70d01b98f92798ce3816709b1c65268db
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-02-24 08:12:49 +0000

    ヘッダーポジション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :blogs
-  validates :name, presence: true
+  has_many :blogs, dependent: :destroy
+  validates :name, presence: true 
 end
