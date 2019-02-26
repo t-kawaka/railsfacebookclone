8790f4b4a97a1a6ebd3d961845c29361bb1d3aa9
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-02-26 11:53:21 +0000

    ボタンなどのレイアウト修正

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Blogger
     config.load_defaults 5.1
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
