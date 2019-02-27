033f82127f72e63c1909d572189872509828b586
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-02-27 08:02:26 +0000

    インデント修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,7 +18,7 @@ class BlogsController < ApplicationController
     @blog.user_id = current_user.id
     if @blog.save
     Blog.create(blog_params)
-      redirect_to new_blog_path, notice:"ブログを作成しました"
+      redirect_to blogs_path, notice:"ブログを作成しました"
     else
       render 'new'
     end
@@ -27,7 +27,6 @@ class BlogsController < ApplicationController
   def confirm
     @blog = Blog.new(blog_params)
   end
-  
   
   def show
   end
