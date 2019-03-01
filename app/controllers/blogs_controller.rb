7e68ba30d3a6533985ac792ccf414aca4d5b6562
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-01 06:05:56 +0000

    Blogs_controller 修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,6 @@ class BlogsController < ApplicationController
     @blog = Blog.new(blog_params)
     @blog.user_id = current_user.id
     if @blog.save
-    Blog.create(blog_params)
       redirect_to blogs_path, notice:"ブログを作成しました"
     else
       render 'new'
